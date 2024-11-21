32f58b954ad048c3b1736972e8c57ee031ded7d4
Diogo Almeida <77116842+wartuga@users.noreply.github.com>
2024-11-21 15:37:23 +0000

    test commit

--- app/src/main/java/com/cmapp/MainActivity.kt
+++ app/src/main/java/com/cmapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -32,6 +33,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column{
+        
+    }
     Text(
         text = "Hello $name!",
         modifier = modifier
