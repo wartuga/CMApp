565aadd3fc60b6d7291b5674dcbe23a73462320c
Bruna Santos <fc56328@alunos.ciencias.ulisboa.pt>
2024-12-18 23:50:21 +0000

    Add basic map structure

--- app/src/main/java/com/cmapp/ui/screens/MapScreen.kt
+++ app/src/main/java/com/cmapp/ui/screens/MapScreen.kt
@@ -1,24 +1,34 @@
 package com.cmapp.ui.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import com.cmapp.R
 import com.cmapp.ui.screens.utils.ScreenSkeleton
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapType
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberCameraPositionState
+import kotlin.random.Random
 
 const val TITLE_SIZE = 28
 const val FONT_SIZE = 16
@@ -86,11 +96,47 @@ private fun MapScreenContent(modifier: Modifier) {
             )
         }
         Row(modifier = modifier.padding(PADDING.dp)) {
-            Image(
-                painter = painterResource(id = R.drawable.map),
-                contentDescription = "Map",
-                modifier = modifier.size(MAP_SIZE.dp)
-            )
+//            Image(
+//                painter = painterResource(id = R.drawable.map),
+//                contentDescription = "Map",
+//                modifier = modifier.size(MAP_SIZE.dp)
+//            )
+
+            //https://mapsplatform.google.com/resources/blog/compose-maps-sdk-android-now-available/
+            var uiSettings by remember { mutableStateOf(MapUiSettings()) }
+            var properties by remember {
+                mutableStateOf(MapProperties(mapType = MapType.SATELLITE))
+            }
+            val fculLoc = LatLng(38.756544413269, -9.155370717573753)
+
+
+            val cameraPositionState = rememberCameraPositionState {
+                position = CameraPosition.fromLatLngZoom(fculLoc, 15f)
+            }
+            val places = remember {
+                generateRandomPlaces(5, fculLoc)
+            }
+            //38.757238, -9.155648
+            //Max distance of a Km: 0.01 degrees
+            GoogleMap(
+                modifier = Modifier.fillMaxSize(),
+                properties = properties,
+                uiSettings = uiSettings,
+                cameraPositionState = cameraPositionState
+            ) {
+                Marker(
+                    position = fculLoc,
+                    title = "Fcul",
+                    snippet = "Marker in Fcul",
+                    icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)
+               )
+                for (place in places)
+                    Marker(
+                        position = place,
+                        title = "Ingredient",
+                        snippet = "Marker in Ingredient"
+                    )
+            }
         }
         Row(modifier = modifier.padding(PADDING.dp)) {
             Text(
@@ -124,4 +170,12 @@ private fun MapScreenContent(modifier: Modifier) {
 @Composable
 fun MapScreenPreview() {
     MapScreen(modifier = Modifier, null)
+}
+//Max distance of a Km: 0.01 degrees
+fun generateRandomPlaces(numberPlaces: Int, location: LatLng): List<LatLng> {
+    return List(numberPlaces) {
+        val randomLatitude = location.latitude + Random.nextDouble(-0.01, 0.01)
+        val randomLongitude = location.longitude + Random.nextDouble(-0.01, 0.01)
+        LatLng(randomLatitude, randomLongitude)
+    }
 }
