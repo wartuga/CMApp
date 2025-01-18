476535e038315f1451732bc5c01b18c80e45d9a8
joaoRebelo31 <joao.guilherme.rebelo@gmail.com>
2025-01-18 02:49:59 +0000

    Search bar
    Alterada search bar no social

--- app/src/main/java/com/cmapp/ui/screens/social/FriendsListScreen.kt
+++ app/src/main/java/com/cmapp/ui/screens/social/FriendsListScreen.kt
@@ -3,9 +3,12 @@ package com.cmapp.ui.screens.social
 import android.content.Context
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,8 +21,13 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,6 +36,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -51,6 +61,7 @@ fun FriendsListScreen(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun FriendsListScreenContent(modifier: Modifier, navController: NavHostController?) {
 
@@ -58,26 +69,53 @@ private fun FriendsListScreenContent(modifier: Modifier, navController: NavHostC
     val friends = listOf("Friend 1", "Friend 2", "Friend 3", "Friend 4", "Friend 5")
 
     val scrollState = rememberScrollState()
+    val searchTextState = remember { androidx.compose.runtime.mutableStateOf(TextFieldValue("")) }
 
     Column(
         modifier = modifier.fillMaxSize().verticalScroll(scrollState),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(modifier = modifier.height(24.dp))
         // Search bar
-        Button(
-            onClick = {  },
+        Box(
             modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .border(BorderStroke(3.dp, Color.White), shape = androidx.compose.foundation.shape.RoundedCornerShape(24.dp))
                 .fillMaxWidth()
-                .padding(16.dp)
         ) {
-            Image(
-                imageVector = Icons.Default.Search,
-                contentDescription = "Search",
-                alignment = Alignment.TopStart
-            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically, // Aligns the icon and text field vertically
+            ) {
+                // Icon
+                Icon(
+                    painter = painterResource(id = R.drawable.magnifying_glass), // Replace with your icon
+                    contentDescription = "Search icon",
+                    tint = Color.White,
+                    modifier = Modifier.padding(start = 16.dp, top = 8.dp, bottom = 8.dp).size(30.dp) // Adds some space between the icon and the text field
+                )
+
+                // Text Input (TextField)
+                TextField(
+                    value = searchTextState.value, // Replace with your TextField value
+                    onValueChange = { newText -> searchTextState.value = newText },
+                    modifier = Modifier.fillMaxWidth(),
+                    singleLine = true,
+                    textStyle = TextStyle(
+                        fontSize = 30.sp,
+                        textDecoration = TextDecoration.Underline,
+                        fontFamily = FontFamily(Font(resId = R.font.harry)),
+                        color = Color.White
+                    ),
+                    colors = TextFieldDefaults.textFieldColors(
+                        containerColor = Color.Transparent, // Makes background transparent
+                        focusedIndicatorColor = Color.Transparent, // Hides the indicator
+                        unfocusedIndicatorColor = Color.Transparent // Hides the indicator when unfocused
+                    )
+                )
+            }
         }
 
-        Spacer(modifier = modifier.height(16.dp))
+        Spacer(modifier = modifier.height(32.dp))
 
         Text(
             text = "FRIEND REQUESTS",
