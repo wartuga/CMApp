30213d3016e1dfb1b63f3def368fab8df0eec772
Bruna Santos <fc56328@alunos.ciencias.ulisboa.pt>
2025-01-21 16:15:19 +0000

    Fix button

--- app/src/main/java/com/cmapp/ui/screens/potions/MapScreen.kt
+++ app/src/main/java/com/cmapp/ui/screens/potions/MapScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -28,10 +29,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.cmapp.R
 import com.cmapp.model.data.DataBaseHelper.getPotion
 import com.cmapp.model.domain.database.Potion
 import com.cmapp.navigation.Screens
@@ -50,8 +55,8 @@ import java.nio.charset.StandardCharsets
 import kotlin.random.Random
 
 const val TITLE_SIZE = 28
-const val FONT_SIZE = 16
-const val MAP_SIZE = 330
+const val FONT_SIZE = 20
+//const val MAP_SIZE = 330 //It takes up the remaining space in the screen
 const val PADDING = 5
 
 @Composable
@@ -286,19 +291,29 @@ private fun MapScreenContent(modifier: Modifier, navController: NavHostControlle
         }
 
         Row {
-            Button(onClick = {
-                potionColor?.let { color ->
-                    val encodedColor = URLEncoder.encode(color, StandardCharsets.UTF_8.toString())
-                    navController!!.navigate(Screens.ColorChecker.route + "?potionColor=$encodedColor")
-                }
-            },
+            Button(
+                onClick = {
+                    potionColor?.let { color ->
+                        val encodedColor = URLEncoder.encode(color, StandardCharsets.UTF_8.toString())
+                        navController!!.navigate(Screens.ColorChecker.route + "?potionColor=$encodedColor")
+                    }
+                },
                 modifier = Modifier
-                    .padding(bottom = 40.dp),
-                border = BorderStroke(1.dp, Color.White)
+                    .padding(bottom = 16.dp),
+                border = BorderStroke(2.dp, Color.White),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(83, 12, 114), // Background color
+                    contentColor = Color.White   // Text/icon color
+                ),
             ) {
-                Text(text = "Validate", color = Color.White, fontSize = 24.sp)
+                Text(
+                    text = "Validate", color = Color.White, style = TextStyle(
+                        fontSize = 24.sp,
+                        fontFamily = FontFamily(Font(resId = R.font.harry)),
+                        color = Color.White
+                    )
+                )
             }
-
         }
     }
 }
